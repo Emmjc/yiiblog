f656e622d245fe1f5a3455412a4c3cd246425375
Emmjc <carinomarcus85@gmail.com>
2025-03-15 00:44:41 +0800

    first step of creating and displaying comments

--- protected/models/Comment.php
+++ protected/models/Comment.php
@@ -33,15 +33,11 @@ class Comment extends CActiveRecord
 	 */
 	public function rules()
 	{
-		// NOTE: you should only define rules for those attributes that
-		// will receive user inputs.
 		return array(
-			array('content, status, author, email, post_id', 'required'),
-			array('status, create_time, post_id', 'numerical', 'integerOnly'=>true),
+			array('content, author, email, post_id', 'required'), // Make sure these fields are required and make sure all array here are passed.
 			array('author, email, url', 'length', 'max'=>128),
-			// The following rule is used by search().
-			// @todo Please remove those attributes that should not be searched.
-			array('id, content, status, create_time, author, email, url, post_id', 'safe', 'on'=>'search'),
+			array('email', 'email'),
+			array('url', 'url'),
 		);
 	}
 
@@ -63,13 +59,13 @@ class Comment extends CActiveRecord
 	public function attributeLabels()
 	{
 		return array(
-			'id' => 'ID',
-			'content' => 'Content',
+			'id' => 'Id',
+			'content' => 'Comment',
 			'status' => 'Status',
 			'create_time' => 'Create Time',
-			'author' => 'Author',
+			'author' => 'Name',
 			'email' => 'Email',
-			'url' => 'Url',
+			'url' => 'Website',
 			'post_id' => 'Post',
 		);
 	}
@@ -116,4 +112,17 @@ class Comment extends CActiveRecord
 	{
 		return parent::model($className);
 	}
+
+	protected function beforeSave()
+	{
+		if(parent::beforeSave())
+		{
+			if($this->isNewRecord)
+				$this->create_time = time(); // Automatically records creation time
+			return true;
+		}
+		else
+			return false;
+	}
+
 }
